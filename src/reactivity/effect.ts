c90a2ff82aef27e69af66fb25fcd2d86fcc2da18
suzheng <zheng.su@amh-group.com>
2021-12-06 23:32:00 +0800

    feat:effect stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,37 @@
+import {extend} from '../shared/index'
+
 export class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true
+  onStop?:()=> void
+  public scheduler: Function | undefined;
 
-  constructor(fn, public scheduler?) {
+  constructor(fn, scheduler?:Function) {
     this._fn = fn;
+    this.scheduler = scheduler;
   }
 
   run() {
     activeEffect = this;
     return this._fn();
   }
+
+  stop() {
+    if(this.active){
+     cleanupEffect(this)
+     if(this.onStop){
+       this.onStop()
+     }
+     this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect){
+  effect.deps.forEach((dep:any) => {
+       dep.delete(effect);
+     });
 }
 
 const targetMap = new Map();
@@ -27,7 +50,10 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+
+  if(!activeEffect) return
   dep.add(activeEffect);
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -47,6 +73,19 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
   //fn
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  // extend
+  extend(_effect,options)
+
+
   _effect.run();
-  return _effect.run.bind(_effect);
+
+  const runner:any = _effect.run.bind(_effect)
+
+  runner.effect = _effect
+
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
