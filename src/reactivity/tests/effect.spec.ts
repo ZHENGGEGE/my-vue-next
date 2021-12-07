b9303d4706685570aaac4b7997529dc177a35d21
suzheng <zheng.su@amh-group.com>
2021-12-07 21:22:23 +0800

    fix:修改stop单测

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -67,8 +67,8 @@ describe("effect", () => {
     obj.prop = 2;
     expect(dummy).toBe(2);
     stop(runner);
-    // obj.prop = 3
-    obj.prop++;
+    obj.prop = 3
+    // obj.prop++;
     expect(dummy).toBe(2);
 
     // stopped effect should still be manually callable
