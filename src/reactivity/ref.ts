adeae8843fe5580e974c46d172d73919761f0ea8
suzheng <593012736@qq.com>
2021-12-15 14:46:13 +0800

    feat:实现ref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,5 +1,38 @@
+import { hasChanged, isObject } from "../shared/index";
+import { isTracking, trackEffects, triggerEffect } from "./effect";
+import { reactive } from "./reactive";
+
 class RefImpl {
-  constructor(value) {}
+  public dep;
+  private _value: any;
+  private _rawValue: any;
+
+  constructor(value) {
+    this._value = convert(value);
+    this.dep = new Set();
+  }
+
+  get value() {
+    trackRefValue(this);
+    return this._value;
+  }
+
+  set value(newValue) {
+    if (hasChanged(newValue, this._value)) {
+      this._value = convert(newValue);
+      triggerEffect(this.dep);
+    }
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep);
+  }
 }
 
 export function ref(value) {
